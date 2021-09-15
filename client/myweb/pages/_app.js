8b6d39a32f3418ec04b9b9cc1c0e8e9f3bce8f0e
KrisMT <ratiodetector@hotmail.com>
2021-09-15 13:38:55 +0700

    [FIX] not commit file

--- client/myweb/pages/_app.js
+++ client/myweb/pages/_app.js
@@ -1,12 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from '../components/Header'
+import client from '../app-graphql'
+
+import { ApolloProvider } from '@apollo/client'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <ApolloProvider client={client}>
     <div>
       <Header />
       <Component {...pageProps} />
     </div>
+    </ApolloProvider>
   )
 }
 
